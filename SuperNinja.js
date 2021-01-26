8948326899d621a3e891d0b5cac783ca4fe33632
xkhateebx <khateebo.new@gmail.com>
2021-01-26 23:50:08 +0200

    superninja

---
+++ SuperNinja.js
@@ -0,0 +1,35 @@
+class Ninja {
+    constructor (name , health , speed , strength){
+        this.name = name;
+        this.health = health;
+        this.speed = 3;
+        this.strength = 3;
+
+    }
+    sayName(){
+        console.log(this.name);
+    }
+
+    showStats(){
+        console.log(this.name ,this.strength , this.speed,this.health );
+    }
+    drinkSake(){
+        console.log(this.health+10);
+    }
+    
+
+}
+class Sensei extends Ninja{
+    constructor(name , wisdom){
+        super(name,200,10,10);
+        this.wisdom=10;
+    }
+
+    speakWisdom(){
+        super.drinkSake();
+    }
+    
+}
+const superSensei = new Sensei("Master Splinter");
+superSensei.speakWisdom();
+superSensei.showStats();
